6030677c247370000c62bf487f35a5171a4658e0
Romarionijim <poppinbring@gmail.com>
2023-07-28 10:17:19 +0300

    async await

--- myCode/asyncAndAjax/asyncAwait.js
+++ myCode/asyncAndAjax/asyncAwait.js
@@ -1,1 +1,11 @@
 //async await with fetch api
+const fetch = require('node-fetch')
+
+const asyncFunc = async (country) => {
+    const response = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+    const responeJson = await response.json()
+    console.log(response)
+    console.log(responeJson)
+}
+
+ asyncFunc('portugal')
--- myCode/asyncAndAjax/eventLoop.js
+++ myCode/asyncAndAjax/eventLoop.js
@@ -2,7 +2,7 @@ console.log('test start')
 setTimeout(() => console.log('0 sec timer'), 0)
 Promise.resolve('resolved promise 1').then(res => console.log(res))
 Promise.resolve('resolved promise 2').then(res => {
-    for (let i = 0; i < 10000000000; i++) {}
+    for (let i = 0; i < 1000000000; i++) {}
     console.log(res)
 })
 console.log('test end')
